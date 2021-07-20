5f64f074a50ae24cf6e65fb452120fe710e5285b
Parin Kataria <7147507+parinck@users.noreply.github.com>
2021-07-20 14:32:41 +0530

    Update cd-reminder.plugin.zsh

--- cd-reminder.plugin.zsh
+++ cd-reminder.plugin.zsh
@@ -1,10 +1,12 @@
 function _cd_remind() {
     local CD_REMINDER_FILE=.cd-reminder
     if [ -f "$CD_REMINDER_FILE" ]; then
+        echo;
         echo "💡CD reminder!";
         cat $CD_REMINDER_FILE 1>&2;
-    else 
-        echo "$FILE does not exist."
+        echo;
+    #else 
+    #    echo "$FILE does not exist."
     fi
 }
 
